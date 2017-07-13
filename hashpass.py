7fc92922d3bd16373dd24a5ef211912794512618
Jakub Svoboda <SvobodaJakub@users.noreply.github.com>
2017-07-13 21:14:58 +0200

    A standalone version with enhancements.
    Goals:
    * Maintain a stateless algorithm.
    * Maintain simplicity such that it can all be reimplemented from memory (except for the wordlist which will hopefully be well-archived on the Internet).
    * Implement an easily memorable algorithm for generation of memorable passwords (intended usecase - when Hashpass is not used as a primary password manager but as a failsafe password manager in case that the user loses their stateful password manager database and forgets the service-specific passwords and loses their devices and access to all data).
    * Implement both in Python (for performance and normal use) and Javascript & HTML (for emergency).
    
    Changes in this commit:
    * The html page works in any modern browser.
    * The python implementation from Stephan Boyer's original README as a standalone Python 3 program.
    * Added an algorithm of generating dice numbers. (An algorithm easy enough so as to be easily remembered - really, it's just adding a counter (0, 1, 2, ...) to the key and getting all the digits from the base64 results, ignoring all non-numerical characters, and then ignoring all digits that aren't on a 6-sided dice.)
    * Added the EFF's large diceware wordlist. (Easily retrievable from the Internet.)
    * Added automatic conversion of the numbers from the generated dice numbers into diceware words.
    
    Overview of the Hashpass-powered deterministic diceware algorithm:
    * Have `domain` and `key` strings as usual (the same as for the original Hashpass).
    * In loop, increase counter `i` from 0 to 40, and for each `i` value, generate a new result of `Hashpass(domain, key + i.toString())`, concatenate the results.
    * From the results, select only digits.
    * From the digits, select only digits 1-6 (those allowable on 6-sided dice).
    * Group the digits by 5 (here starts the regular diceware way of generating passwords).
    * Find the diceware words for the 5-digit digit groups.
    * Print results.
    * Around 7 to 12 words are usually found for `i` in (0, 40).
    * It is also possible to do this by hand using e.g. the existing [Hashpass](https://play.google.com/store/apps/details?id=se.slackers.hashpass) ([GitHub](https://github.com/bysse/hashpass-android)) and [Diceware](https://play.google.com/store/apps/details?id=com.dougsko.diceware) ([GitHub](https://github.com/dougsko/com.dougsko.diceware)) apps for Android in case of dire need.

---
+++ hashpass.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+import base64, getpass, hashlib, sys
+
+# TODO parse arguments properly
+# TODO docstring and documentation
+# TODO proper help with --help argument
+
+if len(sys.argv) >= 2 and sys.argv[1] in ["--help", "-h", "/?"]:
+    print("hashpass.py [--diceware-helper [--show-key]]")
+    exit(0)
+
+domain = input('Domain: ').strip().lower()
+key = getpass.getpass('Key: ')
+
+def com(domain, key):
+    bits = domain + '/' + key
+    bits = bytes(bits, "utf8")
+    for i in range(2 ** 16):
+        bits = hashlib.sha256(bits).digest()
+    password = base64.b64encode(bits)[:16]
+    password = str(password, "utf8")
+    return password
+
+password = com(domain, key)
+print('Password: ' + password)
+
+if len(sys.argv) >= 2 and sys.argv[1] == "--diceware-helper":
+    all_dice_nums = []
+    for i in range(40):
+        key_i = "{}{}".format(str(key), str(i))
+        passwd_i = com(domain, key_i)
+        num_in_passwd_i = [x for x in passwd_i if x.isdigit()]
+        dice_nums = [int(x) for x in num_in_passwd_i if int(x) <= 6 and int(x) > 0]
+        all_dice_nums += dice_nums
+        if len(sys.argv) >= 3 and sys.argv[2] == "--show-key":
+            print("counter = {}, key = {}, pwd = {}, numbers = {}".format(str(i), key_i, passwd_i, str(num_in_passwd_i)))
+        else:
+            print("counter = {}, pwd = {}, numbers = {}".format(str(i), passwd_i, str(num_in_passwd_i)))
+    print("diceware numbers: {}".format(str(all_dice_nums)))
+    print("diceware numbers by groups of 5:")
+
+    diceware_word_dict = {}
+    DICEWARE_FILE_NAME = "eff_large_wordlist.txt"
+    try:
+        with open(DICEWARE_FILE_NAME, "r") as f:
+            for line in f:
+                number, word = line.split()
+                diceware_word_dict[number] = word
+    except:
+        print("Problem accessing {}".format(DICEWARE_FILE_NAME))
+
+    curr_group = ""
+    def print_curr_group(curr_group):
+        if curr_group in diceware_word_dict:
+            print("{} {}".format(curr_group, diceware_word_dict[curr_group]))
+        else:
+            print(curr_group)
+
+    for i in all_dice_nums:
+        if len(curr_group) < 5:
+            curr_group += str(i)
+        else:
+            print_curr_group(curr_group)
+            curr_group = ""
+    if curr_group:
+        print_curr_group(curr_group)
+
+
